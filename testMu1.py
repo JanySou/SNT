2b4235572532c3c2683a6073fcf88773d335d087
BEZIAT11 <50955612+BEZIAT11@users.noreply.github.com>
2019-05-23 16:41:04 +0200

    test1

---
+++ testMu1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = 0.066 * np.arange(11)
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([1, 4, 9, 16, 25])
+plt.plot(x, y, "ro", label="y=f(x)")
+plt.xlabel("xreelle")
+plt.ylabel("yreelle")
+plt.grid()
+plt.legend()
+plt.title("Trajectoire de la balle")
+plt.show()
+
+print("truc")
